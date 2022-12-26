4505e2b10acaa44f5f93f52ab59a9e5a7dd14a5c
Palina <palina.akhramchuk@gmail.com>
2022-12-26 23:59:37 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header/Header'
+import BookCard from './components/BookCard'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+
+        <BookCard/>
 
     </div>
   );
---
+++ src/components/BookCard/BookCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./BookCard.module.css"
+
+const BookCard =()=>{
+    return
+}
+
+export default BookCard;
