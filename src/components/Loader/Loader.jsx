55707d16c35b29c6d995272f3b69547509b30fef
Palina <palina.akhramchuk@gmail.com>
2022-12-29 23:59:07 +0300

    loader empty files add

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div>
+            {/*not connect with anything at that time*/}
+        </div>
+    );
+};
+
+export default Loader;
