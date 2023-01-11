5946e65be6592c6779f3a1f36f4a0a8fbfb448c6
Palina <palina.akhramchuk@gmail.com>
2023-01-11 22:05:57 +0300

    manually bootstrap i (package json file and webstorm helps to install)

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.css';
-//not import bootstrap((((((((((((((((
-// <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous">
-// <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous">
+import 'bootstrap/dist/css/bootstrap.css';
 
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
